fa582b24686d1b52909048df028e4d50dd86e7bb
Mariano Reyes <60166093+MarianoReyes@users.noreply.github.com>
2021-06-28 08:51:15 -0600

    paginas de datos creadas

--- .eleventy.js
+++ .eleventy.js
@@ -3,5 +3,13 @@ module.exports = function (eleventyConfig) {
     
 }
 
+module.exports = function(eleventyConfig){
+
+    eleventyConfig.addPassthroughCopy('css')
+    eleventyConfig.addPassthroughCopy('fonts')
+    eleventyConfig.addPassthroughCopy('images')
+    eleventyConfig.addPassthroughCopy('js')
+}
+
 
 

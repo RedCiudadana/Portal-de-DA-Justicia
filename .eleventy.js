40f683cc05df6b19b2da3893ea5fa08da4e8adde
Mariano Reyes <60166093+MarianoReyes@users.noreply.github.com>
2021-07-14 15:02:42 -0600

    imagenes y estilos

--- .eleventy.js
+++ .eleventy.js
@@ -40,7 +40,7 @@ module.exports = function (eleventyConfig) {
     });
 
     eleventyConfig.addPassthroughCopy('css');
-    //eleventyConfig.addPassthroughCopy('fonts');
+    eleventyConfig.addPassthroughCopy('fonts');
     eleventyConfig.addPassthroughCopy('images');
     eleventyConfig.addPassthroughCopy('js');
 
